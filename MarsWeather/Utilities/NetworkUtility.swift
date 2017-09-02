f3f1bff5d8dd43c22e8b7efdbeccbedc25fbb26e
Kurt Prenger <prengekp@gmail.com>
2017-09-01 23:41:34 -0400

    Adding network utility and calls to API

--- MarsWeather/Utilities/NetworkUtility.swift
+++ MarsWeather/Utilities/NetworkUtility.swift
@@ -6,4 +6,36 @@
 //  Copyright © 2017 My Tiny Sandbox. All rights reserved.
 //
 
+import Alamofire
 import Foundation
+
+class NetworkUtility {
+    
+    private static let latestURL = URL(string: "http://marsweather.ingenology.com/v1/latest/")!
+    private static let archiveURL = URL(string: "http://marsweather.ingenology.com/v1/archive/")!
+    
+    static func getCurrentWeatherData() -> [WeatherData] {
+        Alamofire.request(latestURL).responseJSON { response in
+            guard response.result.isSuccess,
+                let data = response.result.value as? [String : Any],
+                let report = data["report"] as? [String : Any] else { return }
+            
+            print(report)
+        }
+        
+        return []
+    }
+    
+    static func getArchiveWeatherData() -> [WeatherData] {
+        Alamofire.request(archiveURL).responseJSON { response in
+            guard response.result.isSuccess,
+                let data = response.result.value as? [String : Any],
+                let results = data["results"] as? [[String : Any]] else { return }
+            
+            print(results)
+        }
+        
+        return []
+    }
+    
+}
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # MarsWeather
 App to display Mars weather information from the MAAS API
+
+## Setup
+This project uses [Carthage](https://github.com/Carthage/Carthage) to manage dependencies. Once you have cloned the repository, ensure you run `carthage bootstrap` to download the necessary dependencies to build and run the project.
+ 
